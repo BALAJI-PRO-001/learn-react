09cea8086057d80f997060386f16e7f0e083cc9b
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-04 21:56:02 +0530

    feat: add example_1. - Creating Basic React Component.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Example_1 from './quickStart/Example_1';
+
+export default function App() {
+  return (
+    <div>
+      <Example_1 />
+    </div>
+  );
+}
---
+++ src/quickStart/Example_1.jsx
@@ -0,0 +1,8 @@
+
+/* Creating Component */
+
+export default function Example_1() {
+  return (
+    <h1>HI</h1>
+  );
+}
