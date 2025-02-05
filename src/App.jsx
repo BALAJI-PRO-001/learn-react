13acaa161b1f48ba19d64af28472e0e5f6a5c811
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-05 22:03:23 +0530

    chore: add example_2 and example_3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Example_1 from './quickStart/Example_1';
+import Example_2 from './quickStart/Example_2';
+import Example_3 from './quickStart/example_3';
 
 export default function App() {
   return (
     <div>
-      <Example_1 />
+      {/* <Example_1 /> */}
+      {/* <Example_2 /> */}
+      <Example_3 />
     </div>
   );
 }
