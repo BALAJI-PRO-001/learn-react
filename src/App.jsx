9a0ea6b775b3c5632fd24d1e8eba03ba4d46f4ce
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-08 22:10:50 +0530

    feat: add example 6, 7, 8, 9.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Example_2 from './quickStart/Example_2';
 import Example_3 from './quickStart/example_3';
 import Example_4 from './quickStart/Example_4';
 import Example_5 from './quickStart/Example_5';
+import Example_6 from './quickStart/example_6';
+import Example_7 from './quickStart/Example_7';
+import Example_8 from './quickStart/Example_8';
+import Example_9 from './quickStart/Example_9';
+
 
 export default function App() {
   return (
@@ -11,7 +16,11 @@ export default function App() {
       {/* <Example_2 /> */}
       {/* <Example_3 /> */}
       {/* <Example_4 /> */}
-      <Example_5 />
+      {/* <Example_5 /> */}
+      {/* <Example_6 /> */}
+      {/* <Example_7 /> */}
+      {/* <Example_8 /> */}
+      <Example_9 />
     </div>
   );
 }
