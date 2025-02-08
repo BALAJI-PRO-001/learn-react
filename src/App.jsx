ab57812b5894693d3a6fd72a4bd9023ffb0deb22
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-08 08:18:24 +0530

    feat: feat example_4 and 5.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Example_1 from './quickStart/Example_1';
 import Example_2 from './quickStart/Example_2';
 import Example_3 from './quickStart/example_3';
+import Example_4 from './quickStart/Example_4';
+import Example_5 from './quickStart/Example_5';
 
 export default function App() {
   return (
     <div>
       {/* <Example_1 /> */}
       {/* <Example_2 /> */}
-      <Example_3 />
+      {/* <Example_3 /> */}
+      {/* <Example_4 /> */}
+      <Example_5 />
     </div>
   );
 }
