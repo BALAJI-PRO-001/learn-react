d4e67b3caccbf6d551b0eabf390a83d97e48a10d
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-08 22:11:38 +0530

    feat: add responding events example.

---
+++ src/quickStart/Example_8.jsx
@@ -0,0 +1,14 @@
+/* Responding to events */
+
+
+export default function Example_8() {
+  function handleClick() {
+    alert('HI');
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
+}
