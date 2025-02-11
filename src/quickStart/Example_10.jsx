038bba3d7ae5d5112bc0d97e1586b015b9b4670e
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-11 22:17:31 +0530

    feat: add shared state example with useState.

---
+++ src/quickStart/Example_10.jsx
@@ -0,0 +1,41 @@
+
+/* Updating the screen */
+
+import { useState } from "react";
+
+
+function Button({ label, count, onButtonClick }) {
+  return (
+    <button
+      style={{margin: '5px'}}
+      onClick={onButtonClick}
+    >
+      {label}: {count}
+    </button>
+  );
+}
+
+
+export default function Example_10() {
+  const [ count, setCount ] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <>
+      <Button 
+        label={'Button 1'}
+        count={count}
+        onButtonClick={handleClick}
+      />
+
+      <Button 
+        label={'Button 2'}
+        count={count}
+        onButtonClick={handleClick}
+      />
+    </>
+  );
+}
