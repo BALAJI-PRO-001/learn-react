008e5c614ab7dc633c4b7749f686ee9d2205b298
BALAJI-PRO-001 <emailForGIT000000@gmail.com>
2025-02-12 22:12:17 +0530

    feat: add describing ui topic example 1.

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import Example_1 from './quickStart/Example_1';
-import Example_2 from './quickStart/Example_2';
-import Example_3 from './quickStart/example_3';
-import Example_4 from './quickStart/Example_4';
-import Example_5 from './quickStart/Example_5';
-import Example_6 from './quickStart/example_6';
-import Example_7 from './quickStart/Example_7';
-import Example_8 from './quickStart/Example_8';
-import Example_9 from './quickStart/Example_9';
-import Example_10 from './quickStart/Example_10';
+/* 
+  Quick Start 
+  ===========
+*/
+// import Example_1 from './quickStart/Example_1';
+// import Example_2 from './quickStart/Example_2';
+// import Example_3 from './quickStart/example_3';
+// import Example_4 from './quickStart/Example_4';
+// import Example_5 from './quickStart/Example_5';
+// import Example_6 from './quickStart/example_6';
+// import Example_7 from './quickStart/Example_7';
+// import Example_8 from './quickStart/Example_8';
+// import Example_9 from './quickStart/Example_9';
+// import Example_10 from './quickStart/Example_10';
+
+
+/* 
+  Describing the UI
+  =================
+*/
+import Example_1 from './describingTheUI/example_1';
 
 
 export default function App() {
   return (
     <div>
-      {/* <Example_1 /> */}
-      {/* <Example_2 /> */}
-      {/* <Example_3 /> */}
-      {/* <Example_4 /> */}
-      {/* <Example_5 /> */}
-      {/* <Example_6 /> */}
-      {/* <Example_7 /> */}
-      {/* <Example_8 /> */}
-      {/* <Example_9 /> */}
-      <Example_10 /> 
-  </div>
+      {/*
+        Quick Start 
+        ===========
+        <Example_1 />
+        <Example_2 />
+        <Example_3 />
+        <Example_4 />
+        <Example_5 />
+        <Example_6 />
+        <Example_7 />
+        <Example_8 />
+        <Example_9 />
+        <Example_10 /> 
+      */}
+
+      <Example_1 />
+    </div>
   );
 }
